6c32a9eb5c7a1b1e53271d2317d919bb5656d378
Nathan Harold <nharold@google.com>
2018-08-20 11:38:27 -0700

    libril: Encode the Number of MNC Digits in CellIdentity
    Legacy RIL uses an integer to encode the number of
    MNC digits. Because the size is not fixed, leading
    zeroes result in ambiguity in the length of the mnc.
    
    This change adds support for passing the number of
    encoded digits in the most-significant nibble of the
    mnc integer (which is only 10 bits). Thus, on any
    implementation that is 16-bits or wider, the mnc info
    will be properly encoded and decoded with the
    correctly-sized string.
    
    Bug: 111971808
    Test: ril::util::mnc::test
    Change-Id: I24aeba5328a63f80b0d6b25b068bd19160191dff

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,9 +28,14 @@ COMMON_PATH := device/samsung/loganreltexx
 
 # Inherit from common msm8930
 -include device/samsung/msm8930-common/BoardConfigCommon.mk
-
 # Inherit from proprietary vendor
 -include vendor/samsung/loganreltexx/BoardConfigVendor.mk
+
+# Includes
+TARGET_SPECIFIC_HEADER_PATH += $(COMMON_PATH)/include
+
+# The first api level the device has commercially launched on
+PRODUCT_SHIPPING_API_LEVEL := 19
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
