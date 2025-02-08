1829aea20a28c56aee71b95d09a4998226f01750
NotNoelChannel <52098241+NotNoelChannel@users.noreply.github.com>
2025-02-08 10:27:26 +0100

    Set SELinux to permissive

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,7 +37,7 @@ PRODUCT_SHIPPING_API_LEVEL := 19
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 
 # Kernel
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.memcg=true user_debug=22 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1
+BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive androidboot.hardware=qcom androidboot.memcg=true user_debug=22 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_IMAGE_NAME := zImage
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
